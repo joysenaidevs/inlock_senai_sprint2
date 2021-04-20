4a0f3b7bd6e65fddb15b84f3bed53a38c091434a
Joyce <joycessilva17@hotmail.com>
2021-04-20 04:18:24 -0300

    Sql atualizado

--- BD/inlock_01_DDL.sql
+++ BD/inlock_01_DDL.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE inlock_games_manha;
 
 USE inlock_games_manha;
+GO
 
 CREATE TABLE estudios(
 idEstudio int primary key identity,
 nomeEstudio varchar(200));
+GO
 
 CREATE TABLE jogos(
 idJogo int primary key identity,
@@ -13,13 +15,15 @@ descricao varchar(500),
 dataLancamento date,
 valor smallmoney,
 idEstudio int foreign key references estudios);
-
+GO
 CREATE TABLE tiposUsuarios(
 idTipoUsuario int primary key identity,
 titulo varchar(100));
+GO
 
 CREATE TABLE usuarios(
 idUsuario int primary key identity,
 email varchar(200),
 senha varchar(50),
 idTipoUsuario int foreign key references tiposUsuarios)
+GO
--- BD/inlock_03_DQL.sql
+++ BD/inlock_03_DQL.sql
@@ -1,21 +1,26 @@
 USE inlock_games_manha;
 
-SELECT * FROM usuarios
-SELECT * FROM estudios
-SELECT * FROM jogos
-
+SELECT * FROM usuarios;
+GO
+SELECT * FROM estudios;
+GO
+SELECT * FROM jogos;
+GO
 SELECT jogos.nomeJogo AS Jogo, estudios.nomeEstudio AS Estudio
 FROM jogos
 LEFT JOIN estudios
-ON jogos.idEstudio = estudios.idEstudio
-
+ON jogos.idEstudio = estudios.idEstudio;
+GO
 SELECT estudios.nomeEstudio AS Estudio, jogos.nomeJogo AS Jogo
 FROM jogos
 RIGHT JOIN estudios
-ON jogos.idEstudio = estudios.idEstudio
-
+ON jogos.idEstudio = estudios.idEstudio;
+GO
 SELECT * FROM jogos
-WHERE idJogo = '1'
-
+WHERE idJogo = '1';
+GO
 SELECT * FROM estudios
-WHERE idEstudio = '1'
+WHERE idEstudio = '1';
+GO
+SELECT * FROM usuarios
+WHERE email = 'admin@admin.com' and senha = 'admin';
